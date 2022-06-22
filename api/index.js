4681e06c79a036f310836b9b0b4c3944dbd27ebd
LukaBulatovic <luka.games94@gmail.com>
2022-06-22 22:21:20 +0200

    Modifying API for Vercel

--- api/index.js
+++ api/index.js
@@ -9,10 +9,10 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname + '/../', 'public')));
 
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('../routes/index');
 
 app.use('/', indexRouter);
 
